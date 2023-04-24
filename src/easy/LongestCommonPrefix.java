7211dac36b7e06636fa7018c2bd1bc944ed20c59
redmachine1 <ya.mordv@gmail.com>
2023-04-24 18:06:46 +0300

    14. Longest Common Prefix

---
+++ src/easy/LongestCommonPrefix.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        String result = strs[0];
+
+        for(int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(result)) {
+                result = result.substring(0, result.length() - 1);
+            }
+        }
+
+        return result;
+
+    }
+}

ce00e00f00c0f92f525334c5d1c2b15572a1bec0
redmachine1 <ya.mordv@gmail.com>
2023-04-25 16:15:04 +0300

    добавление вспомогательного ListNode

---
+++ src/helper_elements/ListNode.java
@@ -0,0 +1,19 @@
+package helper_elements;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}

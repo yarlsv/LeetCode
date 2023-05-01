12a7a7852b3284fd572566257e6b0bdce009ef98
redmachine1 <ya.mordv@gmail.com>
2023-05-01 21:07:35 +0300

    111. Minimum Depth of Binary Tree

---
+++ src/easy/MinDepth.java
@@ -0,0 +1,18 @@
+package easy;
+
+import helper_elements.TreeNode;
+
+public class MinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
